a16a319f900bfeee93a27ef1374baf8fb6980bf3
lineisha gibson <lineishagibson@gmail.com>
2023-03-20 12:49:23 -0500

    react component added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ import React, {Component} from 'react';
+
+class App extends Component{
+    render() {
+        return(
+            <div>
+                <h1>Hello</h1>
+                <p>My name is Lineisha. I'm a software engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+            </div>
+        );
+    }
+}
+
+export default App;
+
