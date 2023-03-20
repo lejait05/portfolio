8d7aa252fe89fcd7f88b0c3e25cbc818023fe41e
lineisha gibson <lineishagibson@gmail.com>
2023-03-20 18:23:26 -0500

    corrected

--- src/SocialProfiles.js
+++ src/SocialProfiles.js
@@ -5,12 +5,12 @@ import SOCIAL_PROFILES from "./data/socialProfiles";
 class SocialProfile extends Component {
     render() {
         console.log('this.props', this.props);
-        const {link, image} = this.props.SocialProfile;
+        const {link, image} = this.props.socialProfile;
         return (
-            <div style={{display: 'inline-block', width: 300, margin: 10}}>
-                <img src={image} alt='socialProfile' style={{width: 200, height: 120}}/>
-                <a href={link}>{link}</a>
-            </div>
+            <span>
+
+                <a href={link}><img src={image} alt='socialProfile' style={{width: 35, height: 35}}/></a>
+            </span>
         )
     }
 }
@@ -19,12 +19,12 @@ class SocialProfiles extends Component {
     render() {
         return (
             <div>
-                <h2>Social Profiles</h2>
+                <h2>Keep In Touch</h2>
                 <div>
                     {
                         SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
                             return (
-                                <SocialProfile key={SOCIAL_PROFILE.id} SocialProfile={SOCIAL_PROFILE}/>
+                                <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE}/>
                             );
                         })
                     }
