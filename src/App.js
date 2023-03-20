e745a42e708fb21e4a0aa2945b71d593d3e28409
lineisha gibson <lineishagibson@gmail.com>
2023-03-20 15:11:26 -0500

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
  import React, {Component} from 'react';
 
 
-class RegularClass{}
-
- class ComponentClass extends Component{}
-
-
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-
-console.log('regularClassInstance', regularClassInstance);
-console.log('componentClassInstance', componentClassInstance);
 
 
 class App extends Component{
+    constructor() {
+        super();
+        this.state= {displayBio: false};
+    }
     render() {
         return(
             <div>
                 <h1>Hello</h1>
                 <p>My name is Lineisha. I'm a software engineer.</p>
                 <p>I'm always looking forward to working on meaningful projects.</p>
+                {
+                    this.state.displayBio ? (
+                    <div>
+                        <p>I live in San Antonio, and I love the world of coding.</p>
+                        <p>I currently have not found a favorite language, they all flow pretty smoothly.</p>
+                        <p>Besides coding, I am an Army vet. I have my Nursing, Massage, and Esthetician Licences all current and active.</p>
+                        <p>Helping people is my passion!</p>
+                    </div>
+                ) : null
+                }
             </div>
         );
     }
