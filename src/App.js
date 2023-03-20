7f3864d2f7f0d0163e7507be319b40ee412ddc12
lineisha gibson <lineishagibson@gmail.com>
2023-03-20 14:50:33 -0500

    sample classes added to understand components and classes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
  import React, {Component} from 'react';
 
+
+class RegularClass{}
+
+ class ComponentClass extends Component{}
+
+
+const regularClassInstance = new RegularClass();
+const componentClassInstance = new ComponentClass();
+
+console.log('regularClassInstance', regularClassInstance);
+console.log('componentClassInstance', componentClassInstance);
+
+
 class App extends Component{
     render() {
         return(
